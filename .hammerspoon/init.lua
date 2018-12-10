22fed111c179fa03437836f871c28541fc5e02e4
motemen <motemen@gmail.com>
2018-12-10 12:41:50 +0900

    hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,15 @@
+hs.hotkey.bind({"cmd","ctrl"}, "Y", function()
+    hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({"cmd","ctrl"}, "L", function()
+    hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind({"cmd","ctrl"}, "I", function()
+    hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind({"cmd","ctrl"}, "H", function()
+    hs.application.launchOrFocus("Firefox")
+end)
